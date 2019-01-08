1ae135b8fd6c96e23a18f571ac4ddb161e575fa0
Sarah Hosking <sehosking@gmail.com>
2019-01-08 12:04:58 +0100

    "New blog entry strava_data"

--- _posts/2019-01-08-strava_data.md
+++ _posts/2019-01-08-strava_data.md
@@ -7,8 +7,6 @@ instagram_id:
 tags: []
 
 ---
-
-
 # Testing export from jupyter notebook
 
 blah blah blah
@@ -31,23 +29,20 @@ while (end_found == False):
     #Do a HTTP Get - First form the full URL
     act_url = base_url + str(page_num)
     strava_json = urllib2.urlopen(act_url).read()
-    #act_num = len(strava_json) #TO FIX, this num is oversstated
 
     if strava_json != "[]":   #This checks whether we got an empty JSON response and so should end
+        
         #Now we process the JSON
         act_json = json.loads(strava_json)
-        #pp.pprint(act_json)
         page_df = pd.DataFrame(act_json)
-
-        # reduce number of fields in df
-        #page_df = page_df[cols]
         strava_df = strava_df.append(page_df)
+        
         #Set up for next loop
         cum_sum = page_num * 200
-        #print "%d out of %d activities printed" % (cum_sum, act_num)
         print("{} activities printed".format(cum_sum))
         page_num += 1
         strava_json = []
+    
     else:
         end_found = True
 ```
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,16 +1,11 @@
 var store = [{
-        "title": "Strade Bianche Gran Fondo 2018",
-        "excerpt":"When I signed up for Etape du Tour last October–and the 28-week training plan that goes along with it–I knew a few interim cycling events would need to happen, to maintain training momentum and to get me used to negotiating mass starts and crowded roads.Reading my friend Gerry’s race report...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/strade-bianche-gran-fondo-2018/",
-        "teaser":null},{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "http://localhost:4000/jekyll/update/welcome-to-jekyll/",
-        "teaser":null},{
         "title": "This is my first post of 2019",
         "excerpt":"Hello, world!","categories": [],
         "tags": [],
         "url": "http://localhost:4000/hello-world/",
+        "teaser":null},{
+        "title": "This is my 2nd post of 2019",
+        "excerpt":"Testing export from jupyter notebookblah blah blah# here is some code#Open the my_file to usemy_file = open(my_acts,'w')#Loop extracting data. Remember it comes in pagesend_found = Falsepage_num = 1strava_df = pd.DataFrame(columns = cols)#Main loop - Getting all activitieswhile (end_found == False): #Do a HTTP Get - First form the full URL...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/strava_data/",
         "teaser":null}]
