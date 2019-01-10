882c9c70c719f9268edf51b054ef44958467e529
Sarah Hosking <sehosking@gmail.com>
2019-01-10 01:11:05 +0100

    fix: update for theme change

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,6 @@
 var store = [{
-        "title": "This is my first post of 2019",
-        "excerpt":"Hello, world!","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/hello-world/",
-        "teaser":null},{
-        "title": "Strava ride data",
+        "title": "Get Strava ride data with Python & Pandas",
         "excerpt":"After spending the past 2.5 months madly finishing up projects, cycling took a serious back seat. Which is OK, because as you will see, cycling took the driver’s seat for much of 2018.Now that the projects are done (or at least at v1), I’ve been itching to look at my...","categories": [],
-        "tags": [],
+        "tags": ["python","pandas","strava"],
         "url": "http://localhost:4000/strava_ride_data/",
         "teaser":null}]
